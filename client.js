56a34ca7bbda2bb4892e29de9076ab84c3e2b3c9
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-22 12:22:00 -0700

    Move connect function to separate file client.js for modularizing

---
+++ client.js
@@ -0,0 +1,24 @@
+// Require net module
+const net = require('net');
+
+const connect = () => {
+  // Establish connection with game server
+  const connection = net.createConnection({
+    host: '135.23.223.133',
+    port: 50542
+  });
+
+  // Interpret incoming data as text
+  connection.setEncoding('utf8');
+
+  // Handle incoming data
+  connection.on('data', (data) => {
+    console.log(data);
+  });
+
+  return connection;
+};
+
+module.exports = {
+  connect
+};
