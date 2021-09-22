22477759e84c1b2ab968da372fe4a01a163fb033
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-22 16:42:41 -0700

    Add function to handle user input in client.js

--- client.js
+++ client.js
@@ -1,6 +1,29 @@
 // Require net module
 const net = require('net');
 
+// Determines what happens depending on keyboard input
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('Disconnecting ...')
+    console.log('Successfully disconnected from game server')
+    process.exit();
+  }
+};
+
+// Allows terminal to listen for keyboard input
+const setupInput = () => {
+  const stdin = process.stdin;
+
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+// Establish a connection to the game server
 const connect = () => {
   // Establish connection with game server
   const connection = net.createConnection({
@@ -11,9 +34,10 @@ const connect = () => {
   // Interpret incoming data as text
   connection.setEncoding('utf8');
 
-  // Handle successful connection even
+  // Handle successful connection event
   connection.on('connect', () => {
     console.log('Successfully connected to game server');
+    connection.write('Name: TMS');
   });
 
   // Handle incoming data
@@ -25,5 +49,6 @@ const connect = () => {
 };
 
 module.exports = {
-  connect
+  connect,
+  setupInput
 };
