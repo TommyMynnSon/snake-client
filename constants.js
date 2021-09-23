3fb04198144fa7205a6e0b6f68c18b6f0cd32a8b
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-09-22 20:14:29 -0700

    Make directional keys and message keys constants in constants.js

--- constants.js
+++ constants.js
@@ -1,7 +1,22 @@
 const IP = '135.23.223.133';
+
 const PORT = 50542;
+
+const VALID_INPUTS = {
+  // Directional Keys
+  w: 'Move: up',
+  a: 'Move: left',
+  s: 'Move: down',
+  d: 'Move: right',
+
+  // Message Keys
+  i: 'Say: Noob!',
+  o: 'Say: I\'m coming for you!',
+  p: 'Say: I\'m unstoppable!'
+};
 
 module.exports = {
   IP,
-  PORT
+  PORT,
+  VALID_INPUTS
 };
--- input.js
+++ input.js
@@ -1,40 +1,16 @@
+const { VALID_INPUTS } = require('./constants');
+
 // Stores the active TCP connection object
 let connection;
 
 // Determines what happens depending on keyboard input
 const handleUserInput = (key) => {
-  if (key === 'w') {
-    connection.write('Move: up');
-  }
-
-  if (key === 'a') {
-    connection.write('Move: left');
-  }
-
-  if (key === 's') {
-    connection.write('Move: down');
-  }
-
-  if (key === 'd') {
-    connection.write('Move: right');
-  }
-
-  if (key === 'i') {
-    connection.write('Say: Noob!');
-  }
-
-  if (key === 'o') {
-    connection.write('Say: I\'m coming for you ...');
-  }
-
-  if (key === 'p') {
-    connection.write('Say: I\'m unstoppable');
-  }
-
   if (key === '\u0003') {
     console.log('Disconnecting ...')
     console.log('Successfully disconnected from game server')
     process.exit();
+  } else {
+    connection.write(VALID_INPUTS[key]);
   }
 };
 
